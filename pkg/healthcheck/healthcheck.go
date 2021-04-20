26984ab11b5ca54e5b88d9fe3c0c444f33ff3e8a
takutaka <takutaka@pepabo.com>
2021-04-20 21:39:57 +0900

    add healthcheck

---
+++ pkg/healthcheck/healthcheck.go
@@ -0,0 +1,91 @@
+package healthcheck
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/route53"
+	v1 "github.com/takutakahashi/external-route53/api/v1"
+)
+
+func Ensure(h *v1.HealthCheck) (*v1.HealthCheck, error) {
+	mySession := session.Must(session.NewSession())
+	r := route53.New(mySession)
+	var ip, hostname *string = nil, nil
+	if h.Spec.Endpoint.Address != "" {
+		ip = aws.String(h.Spec.Endpoint.Address)
+	}
+	if h.Spec.Endpoint.Hostname != "" {
+		hostname = aws.String(h.Spec.Endpoint.Hostname)
+	}
+	var requestInterval int64
+	if h.Spec.Features.FastInterval {
+		requestInterval = 10
+	} else {
+		requestInterval = 30
+	}
+	id := ""
+	lout, err := r.ListHealthChecks(&route53.ListHealthChecksInput{})
+	if err != nil {
+		return nil, err
+	}
+	for _, res := range lout.HealthChecks {
+		if *res.CallerReference == h.SelfLink {
+			id = *res.Id
+		}
+	}
+	if id == "" {
+
+		out, err := r.CreateHealthCheck(&route53.CreateHealthCheckInput{
+			CallerReference: aws.String(h.SelfLink),
+			HealthCheckConfig: &route53.HealthCheckConfig{
+				EnableSNI:                aws.Bool(true),
+				FailureThreshold:         aws.Int64(int64(h.Spec.FailureThreshold)),
+				FullyQualifiedDomainName: hostname,
+				IPAddress:                ip,
+				ResourcePath:             aws.String(h.Spec.Path),
+				Type:                     aws.String("HTTPS"),
+				Inverted:                 aws.Bool(h.Spec.Invert),
+				Disabled:                 aws.Bool(!h.Spec.Enabled),
+				RequestInterval:          aws.Int64(requestInterval),
+			},
+		})
+		if err != nil {
+			return nil, err
+		}
+		h.Status.ID = *out.HealthCheck.Id
+		return h, nil
+	} else {
+
+		out, err := r.UpdateHealthCheck(&route53.UpdateHealthCheckInput{
+			HealthCheckId:            aws.String(id),
+			EnableSNI:                aws.Bool(true),
+			FailureThreshold:         aws.Int64(int64(h.Spec.FailureThreshold)),
+			FullyQualifiedDomainName: hostname,
+			IPAddress:                ip,
+			ResourcePath:             aws.String(h.Spec.Path),
+			Inverted:                 aws.Bool(h.Spec.Invert),
+			Disabled:                 aws.Bool(!h.Spec.Enabled),
+		})
+		if err != nil {
+			return nil, err
+		}
+		h.Status.ID = *out.HealthCheck.Id
+	}
+	_, err = r.ChangeTagsForResource(&route53.ChangeTagsForResourceInput{
+		ResourceType: aws.String("healthcheck"),
+		ResourceId:   aws.String(h.Status.ID),
+		AddTags: []*route53.Tag{
+			{
+				Key:   aws.String("Name"),
+				Value: aws.String(h.SelfLink)},
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	return h, nil
+}
+
+func Delete(h v1.HealthCheck) error {
+	return nil
+}
---
+++ pkg/healthcheck/healthcheck_test.go
@@ -0,0 +1,55 @@
+package healthcheck
+
+import (
+	"testing"
+
+	route53v1 "github.com/takutakahashi/external-route53/api/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestEnsure(t *testing.T) {
+	type args struct {
+		h *route53v1.HealthCheck
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "ok",
+			args: args{
+				h: &route53v1.HealthCheck{
+					ObjectMeta: v1.ObjectMeta{
+						Name:      "test",
+						Namespace: "test",
+						SelfLink:  "/api/v1/namespaces/test/healthchecks/test",
+					},
+					Spec: route53v1.HealthCheckSpec{
+						Enabled:          true,
+						Invert:           false,
+						Protocol:         route53v1.ProtocolTCP,
+						Port:             443,
+						Path:             "/",
+						FailureThreshold: 1,
+						Endpoint: route53v1.HealthCheckEndpoint{
+							Address:  "120.74.236.156",
+							Hostname: "mf.in.takutakahashi.dev",
+						},
+						Features: route53v1.HealthCheckFeatures{
+							FastInterval: true,
+						},
+					},
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if _, err := Ensure(tt.args.h); (err != nil) != tt.wantErr {
+				t.Errorf("Ensure() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
